7dda7a23e003d95ed0a38182e9d8906e41867354
Boleslav Horak <boleslavh@gmail.com>
2022-03-07 14:37:50 +0100

    Added UI slider

---
+++ Runtime/UI/ConfigUiSlider.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using SeweralIdeas.Config;
+namespace Aop.Core.Config
+{
+    public class ConfigUiSlider : MonoBehaviour
+    {
+        [SerializeField] private Slider m_slider;
+        [SerializeField] private FloatConfigValue m_configValue;
+        private bool m_skipCallbacks = false;
+
+        private void OnEnable()
+        {
+            m_slider.value = m_configValue.Value;
+            m_slider.onValueChanged.AddListener(OnSliderChanged);
+            m_configValue.onValueChanged += OnConfigChanged;
+        }
+
+        private void OnDisable()
+        {
+            m_slider.onValueChanged.RemoveListener(OnSliderChanged);
+            m_configValue.onValueChanged -= OnConfigChanged;
+        }
+
+        private void OnConfigChanged(float newValue)
+        {
+            if (m_skipCallbacks)
+                return;
+            m_skipCallbacks = true;
+            m_slider.value = newValue;
+            m_skipCallbacks = false;
+        }
+
+        private void OnSliderChanged(float newValue)
+        {
+            if (m_skipCallbacks)
+                return;
+            m_skipCallbacks = true;
+            m_configValue.Value = newValue;
+            m_skipCallbacks = false;
+        }
+    }
+
+}
