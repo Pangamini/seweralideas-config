b8dbffafaafffc62713b8fa49c08b7092710ff6b
Bolek <boleslav.horak@games-farm.com>
2024-09-16 15:50:39 +0200

    ADded Config GUI

---
+++ Runtime/Gui/ConfigGuiElement.cs
@@ -0,0 +1,74 @@
+using System;
+using SeweralIdeas.Config;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace SeweralIdeas.ConfigGui
+{
+    public abstract class ConfigGuiElement : MonoBehaviour
+    {
+    }
+    
+    public abstract class ConfigGuiElement<T> : ConfigGuiElement
+    {
+        [SerializeField] private ConfigField<T> m_field;
+        private                  ConfigField<T> m_registeredField;
+        private                  bool           m_enabled;
+
+        public ConfigField<T> Field
+        {
+            get => m_field;
+            set
+            {
+                if (m_field == value)
+                    return;
+                
+                m_field = value;
+                if (m_enabled)
+                    RegisteredField = m_field;
+            }
+        }
+
+        private ConfigField<T> RegisteredField
+        {
+            get => m_registeredField;
+            set
+            {
+                if (m_registeredField != null)
+                    m_registeredField.ValueChanged -= OnFieldValueChanged;
+                m_registeredField = value;
+                if (m_registeredField != null)
+                {
+                    m_registeredField.ValueChanged += OnFieldValueChanged;
+                    OnFieldValueChanged(m_registeredField.Value);
+                }
+            }
+        }
+
+        protected abstract void OnFieldValueChanged(T value);
+
+        protected void OnGuiValueChanged(T guiValue)
+        {
+            m_field.Value = guiValue;
+        }
+        
+        protected void OnEnable()
+        {
+            m_enabled = true;
+            RegisteredField = Field;
+        }
+
+        protected void OnDisable()
+        {
+            RegisteredField = null;
+            m_enabled = false;
+        }
+
+        protected void OnValidate()
+        {
+            if (!m_enabled)
+                return;
+            RegisteredField = m_field;
+        }
+    }
+}
---
+++ Runtime/Gui/ConfigGuiToggle.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SeweralIdeas.ConfigGui
+{
+    public class ConfigGuiToggle : ConfigGuiElement<bool>
+    {
+        [SerializeField] private Toggle m_toggle;
+        
+        protected override void OnFieldValueChanged(bool value)
+        {
+            m_toggle.isOn = value;
+        }
+
+        protected void Awake()
+        {
+            m_toggle.onValueChanged.AddListener(OnGuiValueChanged);
+        }
+
+        protected void OnDestroy()
+        {
+            m_toggle.onValueChanged.RemoveListener(OnGuiValueChanged);
+        }
+    }
+}
